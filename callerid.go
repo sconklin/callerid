cf70a239243356499db1b96407935a5e089b11f6
Steve Conklin <steve@conklinhouse.com>
2018-07-20 17:33:49 -0500

    Add UDP broadcast of received callerID data

--- callerid.go
+++ callerid.go
@@ -3,60 +3,29 @@ package main
 import (
 	/*	"flag" */
 	"fmt"
-	"log"
-	"strings"
-	/*	"unicode" */
 	"github.com/tarm/serial"
-	/*	"net" */ /*"os" */)
+	"log"
+	"net"
+	/*	"os" */
+	"strings"
+)
 
-/*
- * About Caller ID format
- *
- * http://melabs.com/resources/callerid.htm
- *
- * SDMF not shown here since we don't receive it from our provider
- *
- * MDMF - Multiple Data Message Format
- *
- *  Message Type: 0x80 is MDMF
- *  Length of data
- *  [repeat]
- *    Data Type [1: date and time, 2: phone number, 4: number not present, 7: name, 8: name not present
- *    Length of data
- *    data
- *  Checksum
- *
- *
- * And info about MDMF types came from here:
- * http://www.holtek.com.tw/documents/10179/116745/an0053e.pdf
- *
- * 0x01 - Time
- * 0x02 - Calling Line Identification
- * 0x03 - Reserved (for Dialable Directory Number (DN))
- * 0x04 - Reason for absence of DN
- * 0x05 - Reserved (for Reason for Redirection)
- * 0x06 - Call Qualifier
- * 0x07 - Name
- * 0x08 - Reason for absence of name
- * 0x0B - Message waiting notification
- */
-
-/*
-type CIDinfo struct {
-	Name string
-}
-*/
-
-func checksum_valid(msg_type byte, msg_data []byte, msg_len byte, msg_csum byte) bool {
+func checksum_valid(msg_data []byte) bool {
+	/*
+	   type
+	   length
+	   data
+	   checksum
+	*/
+	msg_len := msg_data[1]
 	var sum byte
-	sum = msg_type
-	sum = sum + msg_len
 	var j byte
-	for j = 0; j < msg_len; j++ {
+
+	for j = 0; j < msg_len+3; j++ {
 		sum = sum + msg_data[j]
 	}
 
-	if (sum + msg_csum) == 0 {
+	if (sum) == 0 {
 		return true
 	} else {
 		return false
@@ -64,7 +33,6 @@ func checksum_valid(msg_type byte, msg_data []byte, msg_len byte, msg_csum byte)
 }
 
 func parse_MDMF(msg_data []byte) {
-	/* Data Type [1: date and time, 2: phone number, 4: number not present, 7: name, 8: name not present */
 	var idx int
 	var bleft byte
 	var time string
@@ -131,10 +99,8 @@ func parse_MDMF(msg_data []byte) {
 }
 
 func main() {
-	/*
-		var verbose = flag.Bool("v", false, "Enable verbose output")
-		flag.Parse()
-	*/
+
+	const UDP_BROADCAST_PORT = "15987"
 	const IDLE = 0
 	const SYNC = 1
 	const READING_LENGTH = 2
@@ -150,8 +116,8 @@ func main() {
 	var msg_type byte
 	var msg_len byte
 	var working_len byte
-	msg_data := make([]byte, 256)
-	var msg_csum byte
+	buf := make([]byte, 128)
+	msg_data := make([]byte, 300)
 
 	/* Read the serial data until we receive caller id info */
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 1200}
@@ -162,7 +128,21 @@ func main() {
 	}
 	defer s.Close()
 
-	buf := make([]byte, 128)
+	txport := "255.255.255.255:" + UDP_BROADCAST_PORT
+	TxAddr, err := net.ResolveUDPAddr("udp", txport)
+	if err != nil {
+		log.Fatal(err)
+	}
+	LocalAddr, err := net.ResolveUDPAddr("udp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	TxConn, err := net.DialUDP("udp", LocalAddr, TxAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer TxConn.Close()
 
 	state := IDLE
 	num_sync = 0
@@ -189,9 +169,10 @@ func main() {
 					num_sync = 0
 				}
 			case SYNC:
-				/*				fmt.Printf("Examining: 0x%0X\n", buf[idx]) */
 				msg_type = buf[idx]
 				if msg_type == 0x80 { /* Our BGW210 router/VOIP interface always sends type 0x80 */
+					msg_data[data_idx] = buf[idx]
+					data_idx = data_idx + 1
 					state = READING_LENGTH
 				} else {
 					sync_holding = sync_holding - 1
@@ -203,35 +184,41 @@ func main() {
 				msg_len = buf[idx]
 				if msg_len > 0 {
 					working_len = msg_len
+					msg_data[data_idx] = buf[idx]
+					data_idx = data_idx + 1
 					state = READING_DATA
-					data_idx = 0
 				} else {
 					state = IDLE
+					data_idx = 0
 				}
 			case READING_DATA:
 				msg_data[data_idx] = buf[idx]
-				/*				fmt.Printf("idx = %d, data_idx = %d ", idx, data_idx)
-								fmt.Printf("Data = %d (0x%0x)\n", buf[idx], buf[idx]) */
 				data_idx = data_idx + 1
 				working_len = working_len - 1
 				if working_len == 0 {
 					state = READING_CHECKSUM
 				}
 			case READING_CHECKSUM:
-				msg_csum = buf[idx]
-				/* fmt.Printf("Checksum = %d (0x%0x)\n", msg_csum, msg_csum) */
-				if checksum_valid(msg_type, msg_data, msg_len, msg_csum) {
+				msg_data[data_idx] = buf[idx]
+				data_idx = data_idx + 1
+				if checksum_valid(msg_data) {
 					/* do something */
-					/*					fmt.Printf("CSUM valid, Type = %d, Length = %d (0x%0x)\n", msg_type, msg_len, msg_len) */
-					parse_MDMF(msg_data[:msg_len])
-					/* fmt.Printf("%v\n", msg_data) */
+					parse_MDMF(msg_data[2 : msg_len+2])
+
+					/* Send the whole validated message via UDP */
+					_, err := TxConn.Write(msg_data)
+					if err != nil {
+						log.Fatal(err)
+					}
 				} else {
-					/* fmt.Printf("CSUM NOT valid\n") */
+					fmt.Printf("CSUM NOT valid\n")
 				}
 				state = IDLE
+				data_idx = 0
 			default:
 				fmt.Printf("Invalid State, back to IDLE\n")
 				state = IDLE
+				data_idx = 0
 			}
 			n = n - 1
 			idx = idx + 1
